f7c0f61b2dba5fb0d2bb13f9c4d4ec0690a8d7d2
s112756 <s112756@ap.be>
2022-01-03 14:20:29 +0100

    logo toegevoegd

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
-import { header, logo, shimmer, menubtn, menuicon, navicon, menu, activeHeader } from "./header.module.css";
+import { header, logo, shimmer, menubtn, menuicon, navicon, menu, activeHeader, logoImg } from "./header.module.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Header = () => {
   return (
     <div >
       <div className={header}>
-        <Link to="/" className={`${logo} ${shimmer}`}>iPhone Agency</Link>
+        <Link to="/" className={`${logo} ${shimmer}`}>
+          iPhone Agency
+          <StaticImage className={logoImg} src="../images/iphoneLogo.png" />
+        </Link>
 
         <input className={menubtn} type="checkbox" id="menu-btn" />
         <label className={menuicon} htmlFor="menu-btn"><span className={navicon}></span></label>
